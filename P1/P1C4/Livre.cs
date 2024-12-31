520f8ebb3fd425eec9017660b3af3fe97e47b4ee
Hakim.E <etahraouane.h@gmail.com>
2025-01-01 00:05:49 +0100

    chapitre 1 terminé

--- P1/P1C4/Livre.cs
+++ P1/P1C4/Livre.cs
@@ -5,4 +5,12 @@ public class Livre
     public string Titre { get; set; }
     public string Auteur { get; set; }
     public int NombreDePages { get; set; }
+
+    // Ajouter un constructeur pour initialiser les propriétés non-nullables
+    public Livre()
+    {
+        Titre = string.Empty;
+        Auteur = string.Empty;
+        NombreDePages = 0;
+    }
 }
