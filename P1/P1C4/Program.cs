ebd4bd37ad689b085802f3bc5a552f8af8bcd5f1
Hakim.E <etahraouane.h@gmail.com>
2025-01-01 11:57:55 +0100

    modif

--- P1/P1C4/Program.cs
+++ P1/P1C4/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 // Ajouter la directive using pour le namespace contenant la classe Livre
-using P1.P1C4;
+using P1C4;
 
 // TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
 Livre monLivre = new Livre();
@@ -9,7 +9,8 @@ Livre monLivre = new Livre();
 monLivre.Titre = "axelle la catin";
 monLivre.Auteur = "wesley";
 monLivre.NombreDePages = 20000;
+
 // Afficher le contenu des champs de cet objet 
 Console.WriteLine("Le titre du livre est " + monLivre.Titre);
 Console.WriteLine("Son auteur est " + monLivre.Auteur);
-Console.WriteLine("Il compte " + monLivre.NombreDePages);
+Console.WriteLine("Il compte " + monLivre.NombreDePages + " pages.");
