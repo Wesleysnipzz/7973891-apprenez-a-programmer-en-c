5529a014dd386e84e4ffda80d8a2c16241afe55a
Hakim.E <etahraouane.h@gmail.com>
2025-01-02 23:10:56 +0100

    fin chapitre 3

--- P1/P1C5.1/Program.cs
+++ P1/P1C5.1/Program.cs
@@ -3,13 +3,13 @@ string[] couleurs = new string[5];
 
 
 // TODO : Remplir le tableau avec les couleurs demandées dans le README.md
-
+string[] nombreDeCouleurs = new string[] {"rouge", "jaune", "orange", "vert", "bleu"};
 
 // TODO : Remplacer vert par émeraude dans le tableau
-
+nombreDeCouleurs[3] = "émeraude";
 
 // Afficher le contenu du tableau
-foreach (string couleur in couleurs)
+foreach (string couleur in nombreDeCouleurs)
 {
     Console.WriteLine(couleur);
 }
